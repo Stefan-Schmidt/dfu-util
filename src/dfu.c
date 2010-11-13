8a7512b2379a27153a6b4f91c16574796798cc01
Harald Welte <laforge@gnumonks.org>
2010-11-13 12:42:48 +0100

    Add reference to origin of dfu.c file, its purpose and copyright
    This was missing from the original dfu.c file from dfu-programmer,
    but since we ripped it out of there we should properly attribute it.

--- src/dfu.c
+++ src/dfu.c
@@ -1,7 +1,9 @@
 /*
- * dfu-programmer
- *
+ * low-level DFU message sending routines, originally taken from
  * $Id: dfu.c,v 1.3 2006/06/20 06:28:04 schmidtw Exp $
+ * (part of dfu-programmer).
+ *
+ * (C) 2005-2006 Weston Schmidt <weston_schmidt@alumni.purdue.edu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
